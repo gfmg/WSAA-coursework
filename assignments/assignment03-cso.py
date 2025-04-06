61696fb4a2ebf4e85f2bf130c7b1b96bb9cd3e1a
Guille <guillermo.gonzalez@globalfishingwatch.org>
2025-04-06 20:41:08 +0100

    change file name

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-#Get a deck and substract deckID
+# CSO API URL for the dataset
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
 response = requests.get(url)
 out = response.json()
 
-with open('cso_Exchequer_Account.json', 'w') as json_file:
+with open('cso.json', 'w') as json_file:
     json.dump(out, json_file)
